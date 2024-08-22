7e8313012e1a787829d357f8579e1fef38701231
Guna Sekaran M <79622650+guna-sekar-mano@users.noreply.github.com>
2024-08-23 00:20:11 +0530

    new

---
+++ main.py
@@ -0,0 +1,19 @@
+import streamlit as st
+from langchain_helper import create_vector_db, get_qa_chain
+
+st.title("Courses QA")
+
+
+
+btn = st.button("Create Knowledgebase")
+if btn:
+    create_vector_db()
+
+question = st.text_input("Question: ")
+
+if question:
+    chain = get_qa_chain()
+    response = chain(question)
+
+    st.header("Answer: ")
+    st.write(response["result"])
